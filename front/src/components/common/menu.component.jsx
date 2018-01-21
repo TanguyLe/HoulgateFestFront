bfc0a20f786a1948af2d297e986e147fa00ad1f0
Hugo Raynal <raynalhugo@gmail.com>
2018-01-21 22:02:50 +0100

    moving some components

--- front/src/components/common/menu.component.jsx
+++ front/src/components/common/menu.component.jsx
@@ -7,6 +7,7 @@ const Header = () => (
             <ul>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/shotgun'>Shotgun</Link></li>
+                <li><Link to='/log-in-user'>User</Link></li>
             </ul>
         </nav>
     </header>
