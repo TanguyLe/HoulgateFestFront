cb9d524c748f690e967e6ceb202352b20650f3eb
Hugo Raynal <raynalhugo@gmail.com>
2018-01-21 22:03:30 +0100

    added style folder

---
+++ front/src/style/colors/classics.js
@@ -0,0 +1,3 @@
+export const darkText = "#000000";
+export const normalText = "#383838";
+export const lightText = "#707070";
---
+++ front/src/style/shadows.js
@@ -0,0 +1,3 @@
+export const baseShadow = "0px 3px 6px rgba(0, 0, 0, 0.3)";
+export const insetShadow = "0px 3px 6px rgba(0, 0, 0, 0.3) inset";
+export const cardShadow = "0px 0px 6px rgba(0, 0, 0, 0.3)";
