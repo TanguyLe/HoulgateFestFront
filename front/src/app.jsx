3aab0e13542dc4e9fd695e2ba0692c4590b04401
Hugo Raynal <raynalhugo@gmail.com>
2018-01-21 22:05:45 +0100

    added `user-log-in` route

--- front/src/app.jsx
+++ front/src/app.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import Home from './common/home.component'
-import Dummy from './shotgun/dummy.component'
-import Header from './common/menu.component'
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/common/home.component";
+import Dummy from "./components/shotgun/dummy.component";
+import Header from "./components/common/menu.component";
+import UserLogin from "./containers/UserLogin";
 
 class App extends React.Component {
-    render() {
-        return <BrowserRouter>
-            <div><Header/>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/shotgun" component={Dummy}/>
-                </Switch>
-            </div>
-        </BrowserRouter>
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/user-log-in" component={UserLogin} />
+            <Route path="/shotgun" component={Dummy} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App
+export default App;
