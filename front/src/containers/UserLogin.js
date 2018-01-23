bcd7c598cbf04a15c83790173e52f0567bee6496
tanguy <lefloch.ta@gmail.com>
2018-01-23 01:39:51 +0100

    Correction typo

--- front/src/containers/UserLogin.js
+++ front/src/containers/UserLogin.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import UserLogin from "../components/UserLogin";
 
 // this part is supposed to fetch data from server
 // and corresponding login / signup functions
 
-class WrappedUserLogin extends Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return;
-		<div className="fullHeight fullWidth">
-			Test
-			<UserLogin name={"test"} email={"test@gmail.com"} />
-		</div>;
-	}
+class WrappedUserLogin extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div className="fullHeight fullWidth">
+            Test
+            <UserLogin name={"test"} email={"test@gmail.com"}/>
+        </div>;
+    }
 }
 
 export default WrappedUserLogin;
