3ab44d64208c105ad6c850588eed1055c5a4cde3
tanguy <lefloch.ta@gmail.com>
2018-01-31 11:01:56 +0100

    Test of groundFloor representation

--- front/src/shotgun/Floors/GroundFloor.jsx
+++ front/src/shotgun/Floors/GroundFloor.jsx
@@ -15,62 +15,84 @@ class Floor extends React.Component {
 					width: "400px",
 					border: "2px solid #353535", //TODO choose color (don't change this syntaxe, it's used in my pluggin)
 					display: "grid",
-					gridTemplateColumns: "25% 25% 50%",
-					gridTemplateRows: "50% 25% 25%",
-					justifyItems: "center",
-					alignItems: "center"
+					gridTemplateColumns: "12% 18.5% 6% 30% 33.5%",
+					gridTemplateRows: "19% 20% 18% 43%",
+					justifyItems: "stretch",
+					alignItems: "center",
+					justifyContent: "start",
+					alignContent: "start"
 				}}
 			>
 				<Room
 					position={{
-						columnStart: 1,
-						columnEnd: 3,
-						rowStart: 1,
-						rowEnd: 2
-					}}
-				>
-					Chambre 1
-				</Room>
-				<Room
-					position={{
-						columnStart: 3,
-						columnEnd: 4,
-						rowStart: 1,
-						rowEnd: 2
-					}}
-				>
-					Chambre 2
-				</Room>
-				<Room
-					position={{
-						columnStart: 1,
-						columnEnd: 2,
-						rowStart: 2,
-						rowEnd: 3
-					}}
-				>
-					WC
-				</Room>
-				<Room
-					position={{
 						columnStart: 2,
-						columnEnd: 4,
+						columnEnd: 3,
 						rowStart: 2,
 						rowEnd: 3
 					}}
 				>
-					Escaliers
+					Escalier
+				</Room>
+                <Room
+                    position={{
+                        columnStart: 3,
+                        columnEnd: 4,
+                        rowStart: 2,
+                        rowEnd: 3
+                    }}
+                >
+                    W.C
+				</Room>
+                <Room
+                    position={{
+                        columnStart: 4,
+                        columnEnd: 5,
+                        rowStart: 1,
+                        rowEnd: 2
+                    }}
+                >
+                    Salle de Bain
+				</Room>
+                <Room
+                    position={{
+                        columnStart: 2,
+                        columnEnd: 4,
+                        rowStart: 3,
+                        rowEnd: 4
+                    }}
+                >
+                    Couloir
 				</Room>
 				<Room
 					position={{
-						columnStart: 1,
-						columnEnd: 4,
-						rowStart: 3,
+						columnStart: 4,
+						columnEnd: 6,
+						rowStart: 2,
 						rowEnd: 4
 					}}
 				>
-					Chambre 3
+					Cuisine
 				</Room>
+                <Room
+                    position={{
+                        columnStart: 1,
+                        columnEnd: 5,
+                        rowStart: 4,
+                        rowEnd: 5
+                    }}
+                >
+                    Salle à manger
+                </Room>
+                <Room
+                    position={{
+                        columnStart: 5,
+                        columnEnd: 6,
+                        rowStart: 4,
+                        rowEnd: 5
+                    }}
+                >
+                    Petit Salon
+                </Room>
 			</div>
 		);
 	}
