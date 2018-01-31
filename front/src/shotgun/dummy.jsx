082523d488f954c57460b4ea84e52562506f7b0f
tanguy <lefloch.ta@gmail.com>
2018-01-31 12:20:06 +0100

    Removing test code

--- front/src/shotgun/dummy.jsx
+++ front/src/shotgun/dummy.jsx
@@ -4,10 +4,7 @@ import FirstFloor from "./Floors/FirstFloor"
 import GroundFloor from "./Floors/GroundFloor"
 import SecondFloor from "./Floors/SecondFloor"
 import GardenFloor from "./Floors/Garden.jsx"
-import {villaLesGenets} from "./villaLesGenetsDef"
-import {getNbSeats} from "./roomUtils"
 
-console.log(getNbSeats(villaLesGenets));
 
 class Dummy extends React.Component {
 	render() {
