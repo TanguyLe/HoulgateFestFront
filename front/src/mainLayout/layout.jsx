e2421cff4426676f72d4ff8a1bc1cd24268b4ed4
gbarbereau <gautier.barbereau@gmail.com>
2018-02-11 17:59:52 +0100

    Small cleaning

--- front/src/mainLayout/layout.jsx
+++ front/src/mainLayout/layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 import NavBar from './menu'
 import {leftItems, rightItems} from './menu'
