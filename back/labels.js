4ec4cb6471df8a37fca1d84e105593bd92e47189
tanguy <lefloch.ta@gmail.com>
2018-05-06 15:24:04 +0200

    Translating some labels to french

--- back/labels.js
+++ back/labels.js
@@ -1,6 +1,6 @@
-FAILED_AUTH_MSG = "Authentication failed. ";
+FAILED_AUTH_MSG = "L'authentification a échoué. ";
 
-exports.FAILED_AUTH_NO_USER_MSG = FAILED_AUTH_MSG + "User doesn't exist.";
-exports.FAILED_AUTH_WRONG_PSWD_MSG = FAILED_AUTH_MSG + "Invalid password.";
-exports.FAILED_AUTH_ACCOUNT_UNACTIVATED_MSG = FAILED_AUTH_MSG + "Account not activated.";
-exports.FAILED_AUTH_INVALID_CRED_MSG = FAILED_AUTH_MSG + "Invalid credentials.";
+exports.FAILED_AUTH_NO_USER_MSG = FAILED_AUTH_MSG + "L'utilisateur n'existe pas.";
+exports.FAILED_AUTH_WRONG_PSWD_MSG = FAILED_AUTH_MSG + "Mot de passe invalide.";
+exports.FAILED_AUTH_ACCOUNT_UNACTIVATED_MSG = FAILED_AUTH_MSG + "Le compte n'est pas activé.";
+exports.FAILED_AUTH_INVALID_CRED_MSG = FAILED_AUTH_MSG + "Les informations de connexion sont invalides.";
