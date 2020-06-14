8dadce5a935067737acdc356a9284151a8db2d1d
ZMomo <mh.zeghdoudi@gmail.com>
2020-06-14 17:27:32 +0200

    Adding trips table with buttons to add/create/delete trips

---
+++ src/trip/components/tripsTable.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react'
+import { getCredentials, register, unregister } from '../../login/store'
+
+import { Table, Button } from 'semantic-ui-react'
+import dateFormat from '../../utils/dateFormat'
+
+const TripsTable = ({ trips, isBack }) => {
+    const [title] = useState(isBack ? 'Trajet retour' : 'Trajet Aller')
+    const [credentials, setCredentials] = useState()
+    const [connectedUser, setConnectedUser] = useState()
+
+    useEffect(() => {
+        console.log('did mount: ', credentials)
+        register(setCredentials)
+        setCredentials(getCredentials())
+    }, [])
+     
+    useEffect(() => {
+        console.log('credentials did update: ', credentials)
+        if (credentials) setConnectedUser(credentials.login ? credentials.login : undefined)
+    }, [credentials])
+
+    return (
+        <Table celled striped>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell colSpan='5'>
+                        <>{title}</>
+                        <Button floated='right' primary>Ajouter</Button>
+                    </Table.HeaderCell>
+                    
+                </Table.Row>
+                <Table.Row>
+                    <Table.HeaderCell>Conducteur</Table.HeaderCell>
+                    <Table.HeaderCell>Depart</Table.HeaderCell>
+                    <Table.HeaderCell>Lieu</Table.HeaderCell>
+                    <Table.HeaderCell>Passagers</Table.HeaderCell>
+                    <Table.HeaderCell>Actions</Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+                {trips.map((trip, tripIndex) => (
+                    <Table.Row key={`trip-${tripIndex}`}>
+                        <Table.Cell>{trip.driver.username}</Table.Cell>
+                        <Table.Cell>{dateFormat(trip.start)}</Table.Cell>
+                        <Table.Cell>{trip.adress}</Table.Cell>
+                        <Table.Cell>
+                            <ul>{
+                                Array(trip.seats).fill().map((_, index) => (
+                                    <li key={`passenger-${tripIndex}-${index}`}>{trip.passengers[index] ? trip.passengers[index].username : 'Place libre'}</li>
+                                ))
+                            }</ul>
+                        </Table.Cell>
+                        <Table.Cell>
+                            <Button disabled={connectedUser != trip.driver.username}>Modifier</Button>
+                            <Button disabled={connectedUser != trip.driver.username}>Supprimer</Button>
+                        </Table.Cell>
+                    </Table.Row>
+                ))}
+            </Table.Body>
+        </Table>
+    )
+}
+
+export default TripsTable
