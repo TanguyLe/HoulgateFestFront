b6071bc7baf2a5f8d52c3b67d82e3ca75a4666ae
ZMomo <mh.zeghdoudi@gmail.com>
2020-06-17 10:28:58 +0200

    Formating tripPage.jsx and moving dummy trips data in the global scope

--- src/trip/tripPage.jsx
+++ src/trip/tripPage.jsx
@@ -1,45 +1,45 @@
 import React from "react"
-import TripsTable from './components/tripsTable'
 import { Grid } from 'semantic-ui-react'
 
+import TripsTable from './components/tripsTable'
+
+const DATA = [
+    {
+        driver: {"username": "Tanguy"},
+        start: new Date(),
+        adress: "Paris 18",
+        type: "BACK",
+        seats: 3,
+        "passengers": [{"username": "Baptiste"}, {"username": "Romain"}, {"username": "Othmane"}]
+    },
+    {
+        driver: {"username": "Momo"},
+        start: new Date('01 Jan 1970 00:45:00 GMT'),
+        adress: "Paris 18",
+        type: "BACK",
+        seats: 4,
+        "passengers": [{"username": "Julie"}, {"username": "Hugo"}]
+    },
+    {
+        driver: {"username": "Patrick"},
+        start: new Date('28 Jan 1979 15:30:00 GMT'),
+        adress: "Montélimart",
+        type: "FORTH",
+        seats: 1,
+        "passengers": [{"username": "Léa"}]
+    },
+    {
+        driver: {"username": "Patrick"},
+        start: new Date('05 Jan 1970 00:00:00 GMT'),
+        adress: "Châlons-sur-Saone",
+        type: "BACK",
+        seats: 6,
+        passengers: []
+    }
+]
+
 let TripPage = () => {
-
-    const DATA = [
-        {
-            driver: {"username": "Tanguy"},
-            start: new Date(),
-            adress: "Paris 18",
-            type: "BACK",
-            seats: 3,
-            "passengers": [{"username": "Baptiste"}, {"username": "Romain"}, {"username": "Othmane"}]
-        },
-        {
-            driver: {"username": "Momo"},
-            start: new Date('01 Jan 1970 00:45:00 GMT'),
-            adress: "Paris 18",
-            type: "BACK",
-            seats: 4,
-            "passengers": [{"username": "Julie"}, {"username": "Hugo"}]
-        },
-        {
-            driver: {"username": "Patrick"},
-            start: new Date('28 Jan 1979 15:30:00 GMT'),
-            adress: "Montélimart",
-            type: "FORTH",
-            seats: 1,
-            "passengers": [{"username": "Léa"}]
-        },
-        {
-            driver: {"username": "Patrick"},
-            start: new Date('05 Jan 1970 00:00:00 GMT'),
-            adress: "Châlons-sur-Saone",
-            type: "BACK",
-            seats: 6,
-            passengers: []
-        }
-    ]
-
-    return (<>
+    return (
         <Grid divided='vertically'>
             <Grid.Row columns={2}>
                 <Grid.Column>
@@ -50,7 +50,7 @@ let TripPage = () => {
                 </Grid.Column>
             </Grid.Row>
         </Grid>
-    </>);
+    );
 };
 
 export default TripPage
