bee6487b6e5236a63b58c4338f38034a7c8ce00e
ZMomo <mh.zeghdoudi@gmail.com>
2020-07-23 19:06:17 +0200

    Refactoring tripPage, removing non necessary code

--- src/trip/tripPage.jsx
+++ src/trip/tripPage.jsx
@@ -1,65 +1,21 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Grid } from 'semantic-ui-react'
 
+import TripContextProvider from './TripContext'
 import TripsTable from './components/tripsTable'
 
-const TRIPS = [
-    {
-        driver: {"username": "Tanguy"},
-        start: new Date(),
-        adress: "Paris 18",
-        type: "BACK",
-        seats: 3,
-        "passengers": [{"username": "Baptiste"}, {"username": "Romain"}, {"username": "Othmane"}]
-    },
-    {
-        driver: {"username": "Momo"},
-        start: new Date('01 Jan 1970 00:45:00 GMT'),
-        adress: "Paris 18",
-        type: "BACK",
-        seats: 4,
-        "passengers": [{"username": "Julie"}, {"username": "Hugo"}]
-    },
-    {
-        driver: {"username": "Patrick"},
-        start: new Date('28 Jan 1979 15:30:00 GMT'),
-        adress: "Montélimart",
-        type: "FORTH",
-        seats: 1,
-        "passengers": [{"username": "Léa"}]
-    },
-    {
-        driver: {"username": "Patrick"},
-        start: new Date('05 Jan 1970 00:00:00 GMT'),
-        adress: "Châlons-sur-Saone",
-        type: "BACK",
-        seats: 6,
-        passengers: []
-    }
-]
-
-const USERS = [
-    { value: "Baptiste", text: "Baptiste" },
-    { value: "Romain", text: "Romain" },
-    { value: "Othmane", text: "Othmane" },
-    { value: "Julie", text: "Julie" },
-    { value: "Hugo", text: "Hugo" },
-    { value: "Patrick", text: "Patrick" },
-    { value: "Léa", text: "Léa" },
-    { value: "Tanguy", text: "Tanguy" },
-    { value: "Momo", text: "Momo" }
-]
-
-let TripPage = () => {
+const TripPage = () => {
     return (
-        <Grid padded>
-            <Grid.Column mobile={16} tablet={16} computer={8}>
-                <TripsTable trips={TRIPS.filter(trips => trips.type === 'FORTH')} users={USERS}/>
-            </Grid.Column>
-            <Grid.Column mobile={16} tablet={16} computer={8}>
-                <TripsTable isBack trips={TRIPS.filter(trips => trips.type === 'BACK')} users={USERS}/>
-            </Grid.Column>
-        </Grid>
+        <TripContextProvider>
+            <Grid padded>
+                <Grid.Column mobile={16} tablet={16} computer={8}>
+                    <TripsTable />
+                </Grid.Column>
+                <Grid.Column mobile={16} tablet={16} computer={8}>
+                    <TripsTable isBack />
+                </Grid.Column>
+            </Grid>
+        </TripContextProvider>
     );
 };
 
