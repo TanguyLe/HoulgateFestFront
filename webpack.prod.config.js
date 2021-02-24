0fdc0a1c1af8ffeedce852eb1abff816906e8f8c
tanguy <lefloch.ta@gmail.com>
2021-02-24 22:21:53 +0100

    Updating scoreboard address for 2021 in webpack en vars

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -38,7 +38,7 @@ module.exports = {
             NODE_ENV: "production",
             API_URL: null,
             HAS_STARTED: false,
-            SCORES_BOARD_URL: "https://keepthescore.co/board/wytbkwbpzjr/"
+            SCORES_BOARD_URL: "https://keepthescore.co/board/kecveeapsar/"
         })
     ]
 };
