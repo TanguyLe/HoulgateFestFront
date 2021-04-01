d376b17a4dbb4a590618b7a735e691705f41f129
tanguy <lefloch.ta@gmail.com>
2021-04-01 22:38:42 +0200

    Adding the hover popup on drivers

--- src/trip/components/tripsTable.jsx
+++ src/trip/components/tripsTable.jsx
@@ -1,9 +1,56 @@
 import React, { useState, useContext } from 'react';
-import { Table, Button, Portal, Segment, Header } from 'semantic-ui-react';
+import {Table, Button, Portal, Segment, Header, Popup} from 'semantic-ui-react';
 
 import { TripContext } from '../TripContext';
 import dateFormat from '../../utils/dateFormat';
 import TripsModal from "./tripsModal";
+
+const TripsButtons = (props) => {
+    const { trip, isDriver, isBack, setDeletePopup, handleDeleteTrip, deletePopup } = props;
+
+    const buttons = (
+        <div>
+            <TripsModal mode='edit' initialData={trip} disabled={isDriver} isBack={isBack}>
+                Modifier
+            </TripsModal>
+        <Portal
+            openOnTriggerClick
+            open={deletePopup}
+            trigger={<Button size='mini' disabled={isDriver}  content='Supprimer'/>}
+            onOpen={() => setDeletePopup(true)}
+        >
+            <Segment
+                style={{
+                    "border": "solid 1px #666",
+                    left: '40%',
+                    position: 'fixed',
+                    top: '40%',
+                    zIndex: 1000,
+                    textAlign: "center"
+                }}
+            >
+                <Header>Êtes vous sûr de vouloir supprimer ce trajet ?</Header>
+                <Button size='mini' content='Annuler' onClick={() => setDeletePopup(false)}/>
+                <Button size='mini' content='Confirmer' negative onClick={() => handleDeleteTrip(trip._id)}/>
+            </Segment>
+        </Portal>
+    </div>);
+    return <Table.Cell>
+        {
+            !isDriver ? buttons :
+                <Popup
+                    flowing
+                    trigger={buttons}
+                    position={"top center"}
+                    content={<div style={{"width": "15em"}}>
+                        Le conducteur est le seul à pouvoir modifier son trajet,
+                        prends contact avec lui sur fb si tu veux le rejoindre.
+                    </div>
+                    }
+                />
+        }
+    </Table.Cell>
+};
 
 const TripsTable = ({ isBack }) => {
     const { login, trips, getUserById, deleteTrip } = useContext(TripContext);
@@ -15,7 +62,6 @@ const TripsTable = ({ isBack }) => {
         await deleteTrip(id);
         setDeletePopup(false);
     };
-
     return (
         <Table textAlign="center" size="small" celled striped compact>
             <Table.Header>
@@ -48,34 +94,23 @@ const TripsTable = ({ isBack }) => {
                             <Table.Cell>
                                 <ul>{
                                     Array(trip.seats).fill().map((_, index) => (
-                                        <li key={`passenger-${tripIndex}-${index}`}>{trip.passengers[index] ? getUserById(trip.passengers[index]).username : 'Place libre'}</li>
+                                        <li key={`passenger-${tripIndex}-${index}`}>
+                                            {
+                                                trip.passengers[index] ?
+                                                    getUserById(trip.passengers[index]).username : 'Place libre'
+                                            }
+                                        </li>
                                     ))
                                 }</ul>
                             </Table.Cell>
-                            <Table.Cell>
-                                <TripsModal mode='edit' initialData={trip} disabled={login !== getUserById(trip.driver).username} isBack={isBack}>Modifier</TripsModal>
-                                <Portal
-                                    openOnTriggerClick
-                                    open={deletePopup}
-                                    trigger={<Button size='mini' disabled={login !== getUserById(trip.driver).username}  content='Supprimer'/>}
-                                    onOpen={() => setDeletePopup(true)}
-                                >
-                                    <Segment
-                                        style={{
-                                            "border": "solid 1px #666",
-                                            left: '40%',
-                                            position: 'fixed',
-                                            top: '40%',
-                                            zIndex: 1000,
-                                            textAlign: "center"
-                                        }}
-                                    >
-                                        <Header>Êtes vous sûr de vouloir supprimer ce trajet ?</Header>
-                                        <Button size='mini' content='Annuler' onClick={() => setDeletePopup(false)}/>
-                                        <Button size='mini' content='Confirmer' negative onClick={() => handleDeleteTrip(trip._id)}/>
-                                    </Segment>
-                                </Portal>
-                            </Table.Cell>
+                            <TripsButtons
+                                trip={trip}
+                                isBack={isBack}
+                                isDriver={login !== getUserById(trip.driver).username}
+                                setDeletePopup={setDeletePopup}
+                                deletePopup={deletePopup}
+                                handleDeleteTrip={handleDeleteTrip}
+                            />
                         </Table.Row>
                     ))
                 : null}
