8aa79e08351a5ee0f5dbc956c50bd032f0b0a3c5
Tanguy Le Floch <tanguy.lefloch@lynceus.ai>
2023-05-08 21:47:36 +0200

    Switching to togetzer for trips

--- src/trip/tripPage.jsx
+++ src/trip/tripPage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-import TripContextProvider from "./TripContext";
-import TripsTable from "./components/tripsTable";
-
 const TripPage = () => {
     return (
-        <TripContextProvider>
-            <TripsTable />
-        </TripContextProvider>
+        <iframe
+            style="border:none; width:412px; height:172px"
+            src="https://www.togetzer.com/widget.php?params=YTo0OntzOjExOiJhcHBsaWVkVHlwZSI7czo1OiJldmVudCI7czo0OiJ0eXBlIjtzOjU6ImNhcnJlIjtzOjI6ImlkIjtzOjU6IjM4MDM2IjtzOjc6Im9wdGlvbnMiO086Mjk6IklsbHVtaW5hdGVcU3VwcG9ydFxDb2xsZWN0aW9uIjoxOntzOjg6IgAqAGl0ZW1zIjthOjI6e3M6NToiY29sb3IiO3M6NToiYmxhbmMiO3M6NjoiYm9yZGVyIjtzOjU6ImNhcnJlIjt9fX0=">
+        </iframe>
     );
 };
 
