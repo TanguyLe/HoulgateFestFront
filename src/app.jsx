a5889dc73443644e2b29d3aa2a53fd072bd0427b
Tanguy Le Floch <tanguy.lefloch@veesion.com>
2025-04-06 12:30:01 +0200

    design attempt

--- src/app.jsx
+++ src/app.jsx
@@ -19,41 +19,43 @@ class App extends React.Component {
     render() {
         return (
             <Router>
-                <Switch>
-                    <Route exact path="/">
-                        <DefaultLayout exact component={Homepage} />
-                    </Route>
-                    <Route path="/history">
-                        <DefaultLayout component={HistoryPage} auth />
-                    </Route>
-                    <Route path="/shotgun">
-                        <DefaultLayout component={ShotgunPage} bigContainer auth />
-                    </Route>
-                    <Route path="/trips">
-                        <DefaultLayout component={TripPage} bigContainer auth />
-                    </Route>
-                    <Route path="/scores">
-                        <DefaultLayout component={ScoresPage} bigContainer auth />
-                    </Route>
-                    <Route path="/contact">
-                        <DefaultLayout component={ContactPage} />
-                    </Route>
-                    <Route path="/register">
-                        <DefaultLayout component={SignUpPage} notCo />
-                    </Route>
-                    <Route path="/activateUser">
-                        <DefaultLayout component={ActivateAccount} notCo />
-                    </Route>
-                    <Route path="/passwordReset">
-                        <DefaultLayout component={PasswordResetPage} notCo />
-                    </Route>
-                    <Route path="/createPasswordReset">
-                        <DefaultLayout component={CreatePasswordResetPage} notCo />
-                    </Route>
-                    <Route path="*">
-                        <DefaultLayout component={NotFound} />
-                    </Route>
-                </Switch>
+                <div className="app-container">
+                    <Switch>
+                        <Route exact path="/">
+                            <DefaultLayout exact component={Homepage} />
+                        </Route>
+                        <Route path="/history">
+                            <DefaultLayout component={HistoryPage} auth />
+                        </Route>
+                        <Route path="/shotgun">
+                            <DefaultLayout component={ShotgunPage} bigContainer auth />
+                        </Route>
+                        <Route path="/trips">
+                            <DefaultLayout component={TripPage} bigContainer auth />
+                        </Route>
+                        <Route path="/scores">
+                            <DefaultLayout component={ScoresPage} bigContainer auth />
+                        </Route>
+                        <Route path="/contact">
+                            <DefaultLayout component={ContactPage} />
+                        </Route>
+                        <Route path="/register">
+                            <DefaultLayout component={SignUpPage} notCo />
+                        </Route>
+                        <Route path="/activateUser">
+                            <DefaultLayout component={ActivateAccount} notCo />
+                        </Route>
+                        <Route path="/passwordReset">
+                            <DefaultLayout component={PasswordResetPage} notCo />
+                        </Route>
+                        <Route path="/createPasswordReset">
+                            <DefaultLayout component={CreatePasswordResetPage} notCo />
+                        </Route>
+                        <Route path="*">
+                            <DefaultLayout component={NotFound} />
+                        </Route>
+                    </Switch>
+                </div>
             </Router>
         );
     }
