20c241f2092d876f67c839d53ed6f30a22a82179
Tanguy Le Floch <lefloch.ta@gmail.com>
2025-04-27 18:24:27 +0200

    fix(time): fix time check (#175)
    Co-authored-by: Tanguy Le Floch <tanguy.lefloch@veesion.com>

--- src/utils/miscFcts.js
+++ src/utils/miscFcts.js
@@ -1,4 +1,7 @@
 import { UNKNOWN_ERROR_MSG } from "../labels";
+import { SERVER_ENDPOINT } from "../constants";
+
+export const TIME_URL = SERVER_ENDPOINT + "/time/paris";
 
 export const upCaseFirstLetter = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
@@ -11,8 +14,22 @@ export const objectMap = (object, mapFn) => {
     }, {});
 };
 
+// Simple function to parse a date string in format "DD/MM/YYYY HH:MM:SS"
+export const parseFormattedDate = (dateString) => {
+    const regex = /^(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2}):(\d{2})$/;
+    const match = dateString.match(regex);
+
+    if (match) {
+        const [, day, month, year, hours, minutes, seconds] = match;
+        return new Date(year, month - 1, day, hours, minutes, seconds);
+    }
+
+    return new Date(dateString);
+};
+
 export const getParisDatetimeFromAPI = async () => {
-    return fetch("https://worldtimeapi.org/api/timezone/Europe/Paris")
+    return fetch(TIME_URL)
         .then((response) => response.json())
+        .then((json) => parseFormattedDate(json.time))
         .catch((error) => alert(UNKNOWN_ERROR_MSG + error));
 };
